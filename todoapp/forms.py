36728221780c0b2a0c6fd5567c1b34801c580b67
G Sumanth Kumar <58298958+sumanth824@users.noreply.github.com>
2020-09-28 19:45:22 +0530

    Add files via upload

---
+++ todoapp/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.forms import ModelForm
+from .models import *
+
+class listform(forms.ModelForm):
+    class Meta:
+        model = listapp
+        fields = '__all__'
+
+
+
