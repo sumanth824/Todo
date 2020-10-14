b248b39d8f22d190a13585080f1aaf28167f07ed
G Sumanth Kumar <58298958+sumanth824@users.noreply.github.com>
2020-10-14 17:35:39 +0530

    Update models.py

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class listapp(models.Model):
+    author = models.ForeignKey(User,default=None,on_delete=models.CASCADE)
     content = models.CharField(max_length=40)
     Done = models.BooleanField(default=False)
 
